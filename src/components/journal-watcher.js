3ffd5dc9980b6dfa5e4c623d683f69c1ba38d97d
Simon AM <simon@agius-muscat.net>
2017-11-01 11:12:03 +0100

    Added 3rd party log watcher

--- src/components/journal-watcher.js
+++ src/components/journal-watcher.js
@@ -1,106 +1,45 @@
+import os from 'os';
 import fs from 'fs';
 import path from 'path';
-
-/*
-Basic Elite: Dangerous Journal Watching algorithm:
-1. Given a directory:
-2. Check directory exists and is a directory
-Start loop:
-    3. Check if new file has appeared in directory
-    4. If true, choose that file
-    5. If false, choose last modified file
-    6. Watch for file changes
-    7. For each file change:
-        8. Perform whatever processing is required
-*/
+import { LogWatcher } from 'ed-logwatcher';
 
 export default class JournalWatcher {
     constructor(directory) {
-        this.directory = directory;
-        this.fileNames = fs.readdirSync(this.directory);
-        this.currentFile = path.join(this.directory, this.getLastCreatedFileName());
-        this.lastLine = null;
-        this.watcher = null;
-        this.init();
+        this.directory = directory || path.join(
+            os.homedir(),
+            'Saved Games',
+            'Frontier Developments',
+            'Elite Dangerous'
+        );
+        this.watcher = new LogWatcher(this.directory, 3);
+        this.linesLoaded = 0;
+        this.lastEvent = '';
     }
 
     init() {
-        if (!fs.lstatSync(this.directory).isDirectory()) {
-            console.log(`Error: Path is not a directory: ${this.directory}`);
-        } else {
-            this.fileNames = fs.readdirSync(this.directory);
-            console.log(`${this.fileNames.length} files found: \n${this.fileNames.join(',')}`)
-        }
-    }
-
-    watch() {
-        this.setupNewFile();
-        this.watchFile();
-    }
-
-    setupNewFile() {
-        if (this.newFileFound()) {
-            this.currentFile = path.join(this.directory, this.getLastCreatedFileName());
-        }
-    }
-
-    newFileFound() {
-        // Check if a new file has been created
-        let fileNames = fs.readdirSync(this.directory);
-        let currentFileNames = this.fileNames.slice();
-        // Compare sorted values
-        fileNames.sort();
-        currentFileNames.sort();
-        return fileNames.join(',') !== currentFileNames.join(',');
-    }
-
-    getLastCreatedFileName() {
-        let latestIndex = 0;
-        let latestTime = new Date(0) ;
-        for (let i = 0; i < this.fileNames.length; i++) {
-            let fileName = this.fileNames[i];
-            let fullPath = path.join(this.directory, fileName);
-            let creationTime = fs.statSync(fullPath).ctime;
-            console.log(`${fileName}: ${creationTime}`);
-        }
-        for (let i = 0; i < this.fileNames.length; i++) {
-            let fileName = this.fileNames[i];
-            let fullPath = path.join(this.directory, fileName);
-            let creationTime = fs.statSync(fullPath).ctime;
-            if (creationTime > latestTime) {
-                latestTime = creationTime;
-                latestIndex = i;
-            }
-        }
-        return this.fileNames[latestIndex];
-    }
-
-    watchFile() {
-        console.log(`Watching: ${this.currentFile}`);
-        this.watcher = fs.watch(this.currentFile, (event, filename) => {
-            if (filename) {
-                console.log(`Event: ${event}`);
-                const file = fs.readFileSync(this.currentFile);
-                // console.log('File content at : ' + new Date() + ' is \n' + file);
-                this.parseFileContents(file.toString());
-                // Check for new file
-                this.setupNewFile();
-            } else {
-                console.log('Filename not provided');
-            }
+        console.log(`Initialising watcher for: ${this.directory}`);
+        this.watcher.on('Started', () => {
+            console.log('Started');
+        });
+        this.watcher.on('error', (err) => {
+            this.watcher.stop();
+            console.error(err.stack || err);
+        });
+        this.watcher.on('finished', () => {
+            console.log('Finished');
+            this.onDataLoad();
+        });
+        this.watcher.on('data', (obs) => {
+            // console.log('Data: ');
+            // console.log(obs);
+            // console.log(obs.length);
+            this.linesLoaded += obs.length;
+            this.lastEvent = obs[obs.length - 1].event;
         });
     }
 
-    parseFileContents(data) {
-        const lines = data.split('\n');
-        // console.log(`Lines: ${lines.join(',')}`);
-        // console.log(`Lines found: ${lines.length}`);
-        const newLine = lines[lines.length - 1];
-        if (newLine !== this.lastLine && newLine !== '') {
-            this.lastLine = newLine;
-            console.log(`New line: ${newLine}`);
-        } else {
-            console.log('No new line found');
-        }
+    onDataLoad() {
+        document.getElementById('dataCount').innerText = this.linesLoaded.toString();
+        document.getElementById('lastEvent').innerText = this.lastEvent;
     }
 }
--- src/renderer.js
+++ src/renderer.js
@@ -1,19 +1,30 @@
+import os from 'os';
+import path from 'path';
 import JournalWatcher from './components/journal-watcher';
 
 // Global reference to watcher to stop user from starting more than one
 let watcher;
+let pathInput = document.getElementById('pathInput');
+let pathButton = document.getElementById('pathButton');
 
 const startWatcher = () => {
     if (watcher) {
         // TODO: Show errors on window
-        console.log(`Error: watcher already started for file: ${watcher.path}`);
+        console.log(`Error: watcher already started for file: ${watcher.directory}`);
     } else {
-        const filePath = document.getElementById('pathInput').value;
-        watcher = new JournalWatcher(filePath);
-        console.log('Starting watcher');
-        watcher.watch();
+        watcher = new JournalWatcher(pathInput.value);
+        watcher.init();
     }
 };
 
-// Attach event to button
-document.querySelector('#pathButton').addEventListener('click', startWatcher);
+// Add default directory to input
+let defaultPath = path.join(
+    os.homedir(),
+    'Saved Games',
+    'Frontier Developments',
+    'Elite Dangerous'
+);
+// Set default value to default path in the input field
+pathInput.value = defaultPath;
+// Attach event to button to start watcher
+pathButton.addEventListener('click', startWatcher);

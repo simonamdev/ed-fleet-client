4d8629fba106d2301ace08eb89bf7884bcca6dfb
Simon AM <simon@agius-muscat.net>
2017-11-02 22:27:22 +0100

    Refactored interface up a level of abstraction

--- src/components/journal.js
+++ src/components/journal.js
@@ -1,6 +1,5 @@
 import JournalTracker from './journal/journal-tracker';
 import JournalWatcher from './journal/journal-watcher';
-import JournalInterface from './journal/journal-interface';
 import JournalTransmitter from './journal/journal-transmitter';
 
 export default class Journal {
@@ -9,20 +8,55 @@ export default class Journal {
         this.url = url;
         this.tracker = new JournalTracker();
         this.watcher = new JournalWatcher(this.tracker, this.directory);
-        this.interface = new JournalInterface(this.watcher, this.tracker);
         this.transmitter = new JournalTransmitter(this.url);
+        this.init();
+    }
+
+    init() {
+        let self = this;
+        // Setup references to DOM elements
+        self.dataCountEl = document.getElementById('dataCount');
+        self.lastEventEl = document.getElementById('lastEvent');
+        self.watcherStateEl = document.getElementById('watcherState');
+        // Subscribe to watcher events
+        self.watcher.on('update', () => {
+            self.updateUI();
+        });
+        // TODO: Subscribe to transmission events
+    }
+
+    updateUI() {
+        this.updateEvents();
+    }
+
+    updateEvents() {
+        document.getElementById('dataCount').innerText = this.tracker.getEventCount();
+        document.getElementById('lastEvent').innerText = this.tracker.getLastEvent().event;
     }
 
     startWatcher() {
         this.tracker.setWatcherState(true);
-        this.interface.updateWatcher();
+        this.updateWatcher();
         this.watcher.init();
     }
 
     stopWatcher() {
         this.tracker.setWatcherState(false);
-        this.interface.updateWatcher();
+        this.updateWatcher();
         this.watcher.stop();
+    }
+
+    // TODO: Move into updateUI?
+    updateWatcher() {
+        if (this.tracker.getWatcherState()) {
+            this.watcherStateEl.classList.add('active');
+            this.watcherStateEl.classList.remove('inactive');
+            this.watcherStateEl.innerText = 'Active';
+        } else {
+            this.watcherStateEl.classList.add('inactive');
+            this.watcherStateEl.classList.remove('active');
+            this.watcherStateEl.innerText = 'Inactive';
+        }
     }
 
     checkConnection() {

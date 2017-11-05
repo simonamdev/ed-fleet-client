20b8d6551a3700d0569a3510d682eafa81a91d6f
Simon AM <simon@agius-muscat.net>
2017-11-05 23:16:58 +0100

    Validate settings fields

--- src/renderer.js
+++ src/renderer.js
@@ -2,12 +2,12 @@ import Journal from './components/journal';
 import packageJson from '../package.json';
 import { ipcRenderer } from 'electron';
 
-
+// TODO: Move DOM references into a separate class
 // References to DOM elements
-let pathInput = document.getElementById('pathInput');
 let startButton = document.getElementById('startButton');
 let stopButton = document.getElementById('stopButton');
 
+let pathInput = document.getElementById('pathInput');
 let serverInput = document.getElementById('serverInput');
 let cmdrInput = document.getElementById('cmdrInput');
 let apiInput = document.getElementById('apiInput');
@@ -50,7 +50,6 @@ stopButton.addEventListener('click', () => {
 versionEl.innerText = packageJson.version.toString();
 
 // Setup Modal
-
 // Attach modal events
 settingsButton.addEventListener('click', () => {
     settingsModal.classList.add('is-active');
@@ -60,7 +59,22 @@ modalClose.addEventListener('click', () => {
     settingsModal.classList.remove('is-active');
 });
 
-// TODO: Validate fields are not empty
+// Validate fields that are not empty
+const validateOnContent = (e) => {
+    let element = e.target || e;
+    if (element.value && element.value.length) {
+        element.classList.remove('is-danger');
+    } else {
+        element.classList.add('is-danger');
+    }
+};
+
+// Attach validation events
+[pathInput, serverInput, cmdrInput, apiInput].forEach((inputEl) => {
+    validateOnContent(inputEl);
+    inputEl.addEventListener('input', validateOnContent);
+});
+
 settingsSaveButton.addEventListener('click', () => {
     if (journal) {
         let settings = {

61250575465ab25c427b6e2ba9a9a820c645d85d
Simon AM <purrcat259@users.noreply.github.com>
2017-11-07 17:54:51 +0100

    Retrieve server version from server

--- src/pages/components/about.js
+++ src/pages/components/about.js
@@ -1,12 +1,20 @@
+import url from 'url';
+import Settings from './settings';
 import packageJson from '../../../package.json';
 
 // TODO: Version checks and drawing
 // TODO: Setup server version check on watcher successful connection
 
 export default class About {
+    constructor() {
+        this.settings = new Settings();
+    }
+
     init() {
+        this.settings.loadExistingSettings();
         this.setupDomReferences();
         this.updateClientVersion();
+        this.updateServerVersion();
     }
 
     setupDomReferences() {
@@ -20,6 +28,25 @@ export default class About {
     }
 
     updateServerVersion() {
+        const request = new XMLHttpRequest();
+        request.open('GET', url.resolve(this.settings.url, '/version'), true);
 
+        request.onload = () => {
+            if (request.status >= 200 && request.status < 400) {
+                // Success!
+                const response = JSON.parse(request.responseText);
+                this.serverVersionEl.innerText = response.version;
+            } else {
+                // We reached our target server, but it returned an error
+                console.error(`Error: ${request.responseText}, Code: ${request.status}`);
+            }
+        };
+
+        request.onerror = () => {
+            // There was a connection error of some sort
+            console.error('Request error');
+        };
+
+        request.send();
     }
 }

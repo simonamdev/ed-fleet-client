c33ec66bff47e1dcc863e023cc6560c426a8a92c
Simon AM <purrcat259@users.noreply.github.com>
2017-11-07 15:20:46 +0100

    Moved settings DOM events up to settings page class

--- src/pages/components/settings.js
+++ src/pages/components/settings.js
@@ -2,96 +2,50 @@ import os from 'os';
 import fs from 'fs';
 import path from 'path';
 
-const settingIsInvalid = (val) => {
-    return (val == null || val === '');
-};
+const settingIsInvalid = val => (val == null || val === '');
 
 const defaultUrl = 'http://localhost:3000/';
 const defaultPath = path.join(
     os.homedir(),
     'Saved Games',
     'Frontier Developments',
-    'Elite Dangerous'
+    'Elite Dangerous',
 );
 
 export default class Settings {
     constructor(filePath) {
         this.path = filePath || path.join(
             process.resourcesPath,
-            'settings.json'
+            'settings.json',
         );
+        this.defaultUrl = defaultUrl;
+        this.defaultPath = defaultPath;
         this.settings = {};
-    }
-
-    init() {
-        this.setupDomReferences();
-        this.attachDomEvents();
-        this.loadExistingSettings();
-        this.updateFields();
-        this.updateSaveButton();
-    }
-
-    setupDomReferences() {
-        // Inputs
-        this.pathInputEl = document.getElementById('pathInput');
-        this.serverInputEl = document.getElementById('serverInput');
-        this.cmdrInputEl = document.getElementById('cmdrInput');
-        this.apiInputEl = document.getElementById('apiInput');
-        // Buttons
-        this.saveButtonEl = document.getElementById('settingsSave');
-    }
-
-    attachDomEvents() {
-        // Attach validation events to input elements
-        let inputElements = [
-            this.pathInputEl,
-            this.serverInputEl,
-            this.cmdrInputEl,
-            this.apiInputEl
-        ];
-        let self = this;
-        inputElements.forEach((inputEl) => {
-            self.validateOnContent(inputEl);
-            inputEl.addEventListener('input', this.validateOnContent.bind(this));
-        });
-        // Attach event to save button
-        self.saveButtonEl.addEventListener('click', () => {
-            let settings = {
-                path: self.pathInputEl.value,
-                url: self.serverInputEl.value,
-                commander: self.cmdrInputEl.value,
-                apiKey: self.apiInputEl.value
-            };
-            self.updateSettings(settings);
-            self.saveSettings();
-        });
     }
 
     loadExistingSettings() {
         if (this.areAvailable()) {
-            let settings = JSON.parse(fs.readFileSync(this.path));
+            const settings = JSON.parse(fs.readFileSync(this.path));
             this.updateSettings(settings);
+            console.log(`Loaded settings:\nPath: ${settings.path}\nURL: ${settings.url}\nCommander: ${settings.commander}\nAPI Key: ${settings.apiKey}`);
             return true;
-        } else {
-            console.error('Unable to load existing settings');
-            return false;
         }
+        console.error('Unable to load existing settings');
+        return false;
     }
 
     saveSettings() {
         console.log('Writing settings to file');
-        fs.writeFile(this.path, JSON.stringify(this.settings), (err) => {
-            if (err) {
-                // TODO: Setup proper notification on screen
-                console.error('Unable to save settings to file');
-                console.error(err);
-            } else {
-                // Update contents of button for a short while
-                this.saveButtonEl.innerText = 'Settings saved!';
-                setInterval(() => {
-                    this.saveButtonEl.innerText = 'Save Settings';
-                }, 2500);
-            }
+        return new Promise((resolve, reject) => {
+            fs.writeFile(this.path, JSON.stringify(this.settings), (err) => {
+                if (err) {
+                    // TODO: Setup proper notification on screen
+                    console.error('Unable to save settings to file');
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
         });
     }
 
@@ -100,62 +54,6 @@ export default class Settings {
         // Set defaults if not avaiable
         this.settings.path = this.settings.path || defaultPath;
         this.settings.url = this.settings.url || defaultUrl;
-    }
-
-    updateFields() {
-        // Set defaults if they are unavailable
-        this.pathInputEl.value = this.settings.path || this.settings.path || defaultPath;
-        this.serverInputEl.value = this.settings.url || defaultUrl;
-        this.cmdrInputEl.value = this.settings.commander || '';
-        this.apiInputEl.value = this.settings.apiKey || '';
-        // Update validation styles
-        let inputElements = [
-            this.pathInputEl,
-            this.serverInputEl,
-            this.cmdrInputEl,
-            this.apiInputEl
-        ];
-        inputElements.forEach((inputEl) => {
-            this.validateOnContent(inputEl);
-        });
-    }
-
-    validateOnContent(e) {
-        let element = e.target || e;
-        if (element.value && element.value.length) {
-            element.classList.remove('is-danger');
-        } else {
-            element.classList.add('is-danger');
-        }
-        this.updateSaveButton();
-    }
-
-    // Disable save button if the settings are not all valid
-    updateSaveButton() {
-        if (!this.fieldsAreValid()) {
-            this.saveButtonEl.disabled = true;
-            this.saveButtonEl.innerText = 'Settings Invalid - cannot save to file';
-            this.saveButtonEl.title = 'Some settings are invalid, fill them before starting the watcher';
-        } else {
-            this.saveButtonEl.disabled = false;
-            this.saveButtonEl.innerText = 'Save Settings';
-            this.saveButtonEl.title = '';
-        }
-    }
-
-    fieldsAreValid() {
-        let inputElements = [
-            this.pathInputEl,
-            this.serverInputEl,
-            this.cmdrInputEl,
-            this.apiInputEl
-        ];
-        for (let i = 0; i < inputElements.length; i++) {
-            if (settingIsInvalid(inputElements[i].value)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     areAvailable() {
@@ -168,9 +66,7 @@ export default class Settings {
                 !settingIsInvalid(this.settings.url) &&
                 !settingIsInvalid(this.settings.commander) &&
                 !settingIsInvalid(this.settings.apiKey);
-        } else {
-            return false;
         }
-
+        return false;
     }
 }
--- src/pages/settings-page.js
+++ src/pages/settings-page.js
@@ -1,5 +1,123 @@
 import Settings from './components/settings';
 
+
+const settingIsInvalid = val => (val == null || val === '');
+
+class SettingsPage {
+    constructor() {
+        this.settings = new Settings();
+        this.settings.loadExistingSettings();
+    }
+
+    init() {
+        this.setupDomReferences();
+        this.attachDomEvents();
+        this.updateFields();
+        this.updateSaveButton();
+    }
+
+    setupDomReferences() {
+        // Inputs
+        this.pathInputEl = document.getElementById('pathInput');
+        this.serverInputEl = document.getElementById('serverInput');
+        this.cmdrInputEl = document.getElementById('cmdrInput');
+        this.apiInputEl = document.getElementById('apiInput');
+        // Buttons
+        this.saveButtonEl = document.getElementById('settingsSave');
+    }
+
+    attachDomEvents() {
+        // Attach validation events to input elements
+        const inputElements = [
+            this.pathInputEl,
+            this.serverInputEl,
+            this.cmdrInputEl,
+            this.apiInputEl,
+        ];
+        const self = this;
+        inputElements.forEach((inputEl) => {
+            self.validateOnContent(inputEl);
+            inputEl.addEventListener('input', this.validateOnContent.bind(this));
+        });
+        // Attach event to save button
+        self.saveButtonEl.addEventListener('click', () => {
+            const settings = {
+                path: self.pathInputEl.value,
+                url: self.serverInputEl.value,
+                commander: self.cmdrInputEl.value,
+                apiKey: self.apiInputEl.value,
+            };
+            self.settings.updateSettings(settings);
+            self.settings.saveSettings().then(() => {
+                // Update contents of button for a short while
+                this.saveButtonEl.innerText = 'Settings saved!';
+                setInterval(() => {
+                    this.saveButtonEl.innerText = 'Save Settings';
+                }, 2500);
+            }).catch((err) => {
+                console.error(err);
+            });
+        });
+    }
+
+    validateOnContent(e) {
+        const element = e.target || e;
+        if (element.value && element.value.length) {
+            element.classList.remove('is-danger');
+        } else {
+            element.classList.add('is-danger');
+        }
+        this.updateSaveButton();
+    }
+
+    // Disable save button if the settings are not all valid
+    updateSaveButton() {
+        if (!this.fieldsAreValid()) {
+            this.saveButtonEl.disabled = true;
+            this.saveButtonEl.innerText = 'Settings Invalid - cannot save to file';
+            this.saveButtonEl.title = 'Some settings are invalid, fill them before starting the watcher';
+        } else {
+            this.saveButtonEl.disabled = false;
+            this.saveButtonEl.innerText = 'Save Settings';
+            this.saveButtonEl.title = '';
+        }
+    }
+
+    fieldsAreValid() {
+        const inputElements = [
+            this.pathInputEl,
+            this.serverInputEl,
+            this.cmdrInputEl,
+            this.apiInputEl,
+        ];
+        for (let i = 0; i < inputElements.length; i++) {
+            if (settingIsInvalid(inputElements[i].value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    updateFields() {
+        // Set defaults if they are unavailable
+        const settings = this.settings.settings;
+        this.pathInputEl.value = settings.path || this.settings.defaultPath;
+        this.serverInputEl.value = settings.url || this.settings.defaultUrl;
+        this.cmdrInputEl.value = settings.commander || '';
+        this.apiInputEl.value = settings.apiKey || '';
+        // Update validation styles
+        const inputElements = [
+            this.pathInputEl,
+            this.serverInputEl,
+            this.cmdrInputEl,
+            this.apiInputEl,
+        ];
+        inputElements.forEach((inputEl) => {
+            this.validateOnContent(inputEl);
+        });
+    }
+}
+
 console.log('Initialising Settings page');
-let settings = new Settings();
-settings.init();
+const settingsPage = new SettingsPage();
+settingsPage.init();

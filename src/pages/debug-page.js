ec0aa962e6f09b853c2c35e7a541365e70266c96
Simon AM <simon@agius-muscat.net>
2017-11-20 23:58:42 +0100

    Bound debug inputs to debug requests

--- src/pages/debug-page.js
+++ src/pages/debug-page.js
@@ -1,20 +1,33 @@
 const addMessage = (text) => {
     const message = document.createElement('p');
     message.innerText = text;
-    document.getElementById('debug').appendChild(message);
+    const debugEl = document.getElementById('debug');
+    debugEl.appendChild(message);
+    debugEl.scrollTop = debugEl.scrollHeight;
 };
 
 const sendTestEvent = () => {
     const request = new XMLHttpRequest();
     request.open('POST', 'http://localhost:3000/event', true);
     request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    request.setRequestHeader('API-KEY', 'test');
+    request.setRequestHeader('API-KEY', document.getElementById('apiInput').value);
+
+    let debugData = {
+        data: {
+            event: 'TEST DEBUG EVENT',
+            debug: true,
+            starSystem: document.getElementById('systemInput').value
+        },
+        commander: document.getElementById('nameInput').value,
+        timestamp: new Date(),
+    };
 
     request.onload = () => {
         if (request.status >= 200 && request.status < 400) {
             // Success!
             const response = JSON.parse(request.responseText);
-            addMessage(JSON.stringify(response.data));
+            addMessage(`Request: ${JSON.stringify(debugData)}`);
+            addMessage(`Response: ${JSON.stringify(response.data)}`);
         } else {
             // We reached our target server, but it returned an error
             console.error(`Error: ${request.responseText}, Code: ${request.status}`);
@@ -28,17 +41,7 @@ const sendTestEvent = () => {
         addMessage('Error sending debug event');
     };
 
-    request.send(JSON.stringify(
-        {
-            data: {
-                event: 'TEST DEBUG EVENT',
-                debug: true,
-                starSystem: 'Test System'
-            },
-            commander: 'Test CMDR',
-            timestamp: new Date(),
-        },
-    ));
+    request.send(JSON.stringify(debugData));
 };
 
 document.getElementById('eventDebugButton').addEventListener('click', sendTestEvent);
